a35f722621d16a412f077c22a571c9c1c6d69e48
Christo du Toit <christo@dutoit.co.uk>
2023-01-21 10:59:59 +0000

    CONTROLLERS: POST Student

---
+++ POC.SPAL.Api/Controllers/StudentsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using POC.SPAL.Api.Models.Students;
+using POC.SPAL.Api.Models.Students.Exceptions;
+using POC.SPAL.Api.Services.Foundations.Students;
+
+namespace POC.SPAL.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : RESTFulController
+    {
+        private readonly IStudentService studentService;
+
+        public StudentsController(IStudentService studentService) =>
+            this.studentService = studentService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Student>> PostStudentAsync(Student student)
+        {
+            try
+            {
+                Student addedStudent =
+                    await this.studentService.AddStudentAsync(student);
+
+                return Created(addedStudent);
+            }
+            catch (StudentValidationException studentValidationException)
+            {
+                return BadRequest(studentValidationException.InnerException);
+            }
+            catch (StudentDependencyValidationException studentValidationException)
+                when (studentValidationException.InnerException is InvalidStudentReferenceException)
+            {
+                return FailedDependency(studentValidationException.InnerException);
+            }
+            catch (StudentDependencyValidationException studentDependencyValidationException)
+               when (studentDependencyValidationException.InnerException is AlreadyExistsStudentException)
+            {
+                return Conflict(studentDependencyValidationException.InnerException);
+            }
+            catch (StudentDependencyException studentDependencyException)
+            {
+                return InternalServerError(studentDependencyException);
+            }
+            catch (StudentServiceException studentServiceException)
+            {
+                return InternalServerError(studentServiceException);
+            }
+        }
+    }
+}
