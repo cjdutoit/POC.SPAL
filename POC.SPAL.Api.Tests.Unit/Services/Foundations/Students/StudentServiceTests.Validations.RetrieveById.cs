ff3bc1beaefef17a1f7bf9d7ca15a542ba03dfe8
Christo du Toit <christo@dutoit.co.uk>
2023-01-21 10:57:59 +0000

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfStudentIsNotFoundAndLogItAsync -> FAIL

--- POC.SPAL.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.RetrieveById.cs
+++ POC.SPAL.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.RetrieveById.cs
@@ -51,5 +51,47 @@ namespace POC.SPAL.Api.Tests.Unit.Services.Foundations.Students
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfStudentIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someStudentId = Guid.NewGuid();
+            Student noStudent = null;
+
+            var notFoundStudentException =
+                new NotFoundStudentException(someStudentId);
+
+            var expectedStudentValidationException =
+                new StudentValidationException(notFoundStudentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noStudent);
+
+            //when
+            ValueTask<Student> retrieveStudentByIdTask =
+                this.studentService.RetrieveStudentByIdAsync(someStudentId);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(
+                    retrieveStudentByIdTask.AsTask);
+
+            //then
+            actualStudentValidationException.Should().BeEquivalentTo(expectedStudentValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ POC.SPAL.Api/Models/Students/Exceptions/NotFoundStudentException.cs
@@ -0,0 +1,12 @@
+using System;
+using Xeptions;
+
+namespace POC.SPAL.Api.Models.Students.Exceptions
+{
+    public class NotFoundStudentException : Xeption
+    {
+        public NotFoundStudentException(Guid studentId)
+            : base(message: $"Couldn't find student with studentId: {studentId}.")
+        { }
+    }
+}
