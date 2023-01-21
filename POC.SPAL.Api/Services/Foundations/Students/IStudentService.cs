e08323b9255dc7fae3cdb1472874aca3aa12b7b2
Christo du Toit <christo@dutoit.co.uk>
2023-01-21 10:57:57 +0000

    ShouldRetrieveStudentByIdAsync -> FAIL

--- POC.SPAL.Api/Services/Foundations/Students/IStudentService.cs
+++ POC.SPAL.Api/Services/Foundations/Students/IStudentService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using POC.SPAL.Api.Models.Students;
@@ -8,5 +9,6 @@ namespace POC.SPAL.Api.Services.Foundations.Students
     {
         ValueTask<Student> AddStudentAsync(Student student);
         IQueryable<Student> RetrieveAllStudents();
+        ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId);
     }
 }
--- POC.SPAL.Api/Services/Foundations/Students/StudentService.cs
+++ POC.SPAL.Api/Services/Foundations/Students/StudentService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using POC.SPAL.Api.Brokers.DateTimes;
@@ -33,5 +34,8 @@ namespace POC.SPAL.Api.Services.Foundations.Students
 
         public IQueryable<Student> RetrieveAllStudents() =>
             TryCatch(() => this.storageBroker.SelectAllStudents());
+
+        public ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId) =>
+            throw new NotImplementedException();
     }
 }
